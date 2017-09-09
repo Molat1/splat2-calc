2af605da104a2e1044d3372e0d8b1e751942ce65
Molat1 <31802899+Molat1@users.noreply.github.com>
2017-09-09 22:01:04 +0200

    Update es.js

--- locale/es.js
+++ locale/es.js
@@ -12,119 +12,119 @@
 
 var es_strings = {
   // Gear section
-  "HAT_SLOT": "Head",
-  "CLOTHES_SLOT": "Clothes",
-  "SHOES_SLOT": "Shoes",
+  "HAT_SLOT": "Accesorios",
+  "CLOTHES_SLOT": "Ropa",
+  "SHOES_SLOT": "Calzado",
 
   // Stats section
-  "STATS_HEADER": "Stats",
+  "STATS_HEADER": "Estadísticas",
   "INACCURATE_DISCLAIMER": "Stat is inaccurate due to lack of data.",
-  "BOMB_HEADER": "Bomb Damage",
+  "BOMB_HEADER": "Daño por bombas",
   // Will be used in tables - keep these short
-  "BOMB_TABLE_BOMB": "Bomb",
-  "TABLE_DIRECT": "Direct",
-  "TABLE_NEAR": "Near",
-  "TABLE_FAR": "Far",
-  "TABLE_HIT": "Hit",
-  "SPECIAL_HEADER": "Special Damage",
-  "SPECIAL_TABLE_SPECIAL": "Special",
+  "BOMB_TABLE_BOMB": "Tipo de bomba",
+  "TABLE_DIRECT": "Directo",
+  "TABLE_NEAR": "Cercano",
+  "TABLE_FAR": "Lejano",
+  "TABLE_HIT": "Directo",
+  "SPECIAL_HEADER": "Daño por especial",
+  "SPECIAL_TABLE_SPECIAL": "Especiales",
 
   // Footer
-  "FOOTER_COPYRIGHT": "Splatoon and all related copyrights/assets used on this page are the property of Nintendo.",
+  "FOOTER_COPYRIGHT": "Splatoon y todos los derechos de autor / bienes relacionados utilizados en esta página son propiedad de Nintendo.",
   "FOOTER_ABOUT": "about",
 
   // Misc. Strings
-  "UNAVAILABLE": "Unavailable",
-  "UNRELEASED": "Unreleased",
+  "UNAVAILABLE": "No disponible",
+  "UNRELEASED": "No liberado",
   //// Will be used in tables - keep these short
-  "KID_FORM": "Kid",
-  "SQUID_FORM": "Squid",
+  "KID_FORM": "Inkling",
+  "SQUID_FORM": "Calamar",
 
   // Popup headers/buttons
-  "UI_WEAPON_PICKER": "Weapon Picker",
-  "UI_GEAR_PICKER": "Gear Picker",
+  "UI_WEAPON_PICKER": "Arsenal",
+  "UI_GEAR_PICKER": "Ropa",
   "UI_ABOUT": "About",
-  "UI_CHANGELOG": "What's New?",
+  "UI_CHANGELOG": "¿Las novedades?",
   "UI_OK": "OK",
-  "UI_CANCEL": "Cancel",
-  "UI_CONFIRM_CASUAL": "Got it!",
+  "UI_CANCEL": "Cancelar",
+  "UI_CONFIRM_CASUAL": "Vale",
 
   // Tooltips for utility buttons
-  "UI_COPY_LINK": "Copy Link",
-  "UI_SCREENSHOT_MODE": "Toggle Screenshot Mode",
-  "UI_CLEAR_ABILITIES": "Clear Abilities",
-  "UI_CLEAR_ALL": "Clear All",
+  "UI_COPY_LINK": "Copiar enlace",
+  "UI_SCREENSHOT_MODE": "Pantalla Completa",
+  "UI_CLEAR_ABILITIES": "Limpiar potenciadores",
+  "UI_CLEAR_ALL": "Limpiar todo",
   // Tooltips for special icons
-  "UI_NONSTANDARD_SPLATNET": "Non-standard. Only on SplatNet",
-  "UI_NOT_POSSIBLE": "Not possible with selected main",
+  "UI_NONSTANDARD_SPLATNET": "Solo disponible en SplatNet2",
+  "UI_NOT_POSSIBLE": "No es posible obtenerlo con este potenciador",
 
   // Stats - these appear over the stat bars.
   // If there is an asterisk (*), leave it in after the stat name.
-  "STAT_SWIM_SPEED": "Swim Speed",
-  "STAT_RUN_SPEED": "Run Speed",
-  "STAT_RUN_SPEED_RESIST": "Run Speed (Enemy Ink)",
-  "STAT_RUN_SPEED_FIRING": "Run Speed (Firing)",
-  "STAT_RUN_SPEED_ROLLING": "Run Speed (Rolling)",
-  "STAT_RECOVERY_SQUID": "Ink Recovery Speed (Squid)",
-  "STAT_RECOVERY_KID": "Ink Recovery Speed (Kid)",
-  "STAT_SAVER_MAIN": "Ink Consumption (Main)",
-  "STAT_SAVER_SUB": "Ink Consumption (Sub)",
-  "STAT_SPECIAL_CHARGE": "Special Charge Speed",
-  "STAT_SPECIAL_SAVER": "Special Saved *",
-  "STAT_SPECIAL_POWER": "Special Power",
-  "STAT_SPECIAL_POWER_UNKNOWN": "Special Power (???)",
-  "STAT_SPECIAL_POWER_DURATION": "Special Power (Duration)",
-  "STAT_SPECIAL_POWER_BALLER": "Special Power (Baller HP)",
-  "STAT_SPECIAL_POWER_TENTA": "Special Power (Reticule Size)",
-  "STAT_SPECIAL_POWER_SPLASHDOWN": "Special Power (Splash Diameter)",
-  "STAT_SPECIAL_POWER_BUBBLE": "Special Power (Bubble Radius)",
-  "STAT_SUB_POWER": "Sub Power",
-  "STAT_SUB_POWER_RANGE": "Sub Power (Bomb Range)",
-  "STAT_SUB_POWER_CURLING": "Sub Power (Bomb Speed)",
-  "STAT_SUB_POWER_MINE": "Sub Power (Mine Radius)",
-  "STAT_SUB_POWER_WALL": "Sub Power (Wall HP)",
-  "STAT_SUB_POWER_SPRINKLER": "Sub Power (Full-Power Duration)",
-  "STAT_SUB_POWER_BEAKON": "Sub Power (Jump Speed to Beakon)",
-  "STAT_JUMP_SQUID": "Super Jump Time (Squid)",
-  "STAT_JUMP_KID": "Super Jump Time (Kid)",
-  "STAT_QUICK_RESPAWN": "Quick Respawn Time",
-  "STAT_QUICK_RESPAWN_PUNISHER": "Quick Respawn Time *",
-  "STAT_TRACKING_TIME": "Tracking Time",
+  "STAT_SWIM_SPEED": "Velocidad de buceo",
+  "STAT_RUN_SPEED": "Velocidad de carrera",
+  "STAT_RUN_SPEED_RESIST": "Velocidad de desplazamiento (Tinta enemiga)",
+  "STAT_RUN_SPEED_FIRING": "Velocidad de desplazamiento (Disparando)",
+  "STAT_RUN_SPEED_ROLLING": "Velocidad de desplazamiento (Arrastrando)",
+  "STAT_RECOVERY_SQUID": "Recarga Rápida (Calamar)",
+  "STAT_RECOVERY_KID": "Recarga Rápida (Inkling)",
+  "STAT_SAVER_MAIN": "Tintahorro principal",
+  "STAT_SAVER_SUB": "Tintahorro secundario",
+  "STAT_SPECIAL_CHARGE": "Recarga especial",
+  "STAT_SPECIAL_SAVER": "Reducción Especial *",
+  "STAT_SPECIAL_POWER": "Superarma Especial",
+  "STAT_SPECIAL_POWER_UNKNOWN": "Superarma especial (???)",
+  "STAT_SPECIAL_POWER_DURATION": "Superarma especial (Duración)",
+  "STAT_SPECIAL_POWER_BALLER": "Superarma especial (Vida de la esfera)",
+  "STAT_SPECIAL_POWER_TENTA": "Superarma especial (Aumento de la mira)",
+  "STAT_SPECIAL_POWER_SPLASHDOWN": "Superarma especial (Diámetro de Salpicadura)",
+  "STAT_SPECIAL_POWER_BUBBLE": "Superarma especial (Tamaño de la Burbuja)",
+  "STAT_SUB_POWER": "Superarma secundaria",
+  "STAT_SUB_POWER_RANGE": "Superarma secundaria (Rango de la bomba)",
+  "STAT_SUB_POWER_CURLING": "Superarma secundaria (Velocidad de la bomba)",
+  "STAT_SUB_POWER_MINE": "Superarma secundaria (Radio de la bomba trampa)",
+  "STAT_SUB_POWER_WALL": "Superarma secundaria (Vida del telón)",
+  "STAT_SUB_POWER_SPRINKLER": "Superarma secundaria (Dura más en su primera fase)",
+  "STAT_SUB_POWER_BEAKON": "Superarma secundaria (Velocidad de salto a Balizas)",
+  "STAT_JUMP_SQUID": "Supersalto Rápido (Calamar)",
+  "STAT_JUMP_KID": "Supersalto rápido (Inkling)",
+  "STAT_QUICK_RESPAWN": "Retorno exprés",
+  "STAT_QUICK_RESPAWN_PUNISHER": "Tiempo de reaparición (Castigo Póstumo) *",
+  "STAT_TRACKING_TIME": "Tiempo de rastreo",
 
   // Labels that go on the stat bars themselves. Keep these SHORT!
   "LABEL_DISTANCE_PER_FRAME": "{{value}} DU/f",
   "LABEL_PERCENT": "{{value}}%",
   // LABEL_MAIN_COST is combined with "SHOT_UNIT" values below
-  "LABEL_MAIN_COST": "{{value}}% tank/{{unit}}",
-  "LABEL_SUB_COST": "{{value}}% tank",
+  "LABEL_MAIN_COST": "{{value}}% tanque/{{unit}}",
+  "LABEL_SUB_COST": "{{value}}% tanque",
   "LABEL_TIME": "{{value}}s",
   "LABEL_HP": "{{value}} HP",
 
   // Tooltips for stats
-  "DESC_PUNISHER_DISCLAIMER": "Effects with Respawn Punisher aren't fully understood.",
-  "DESC_TRACKING": "Point Sensor/Ink Mine duration",
-  "DESC_RECOVERY_TIME": "{{value}}s from empty to full",
-  "DESC_MAIN_COST": "{{totalShots}} to empty ({{reduction}}% reduction)",
-  "DESC_SUB_COST": "{{reduction}}% reduction",
-  "DESC_SPECIAL_COST": "{{value}}p for special",
+  "DESC_PUNISHER_DISCLAIMER": "Los efectos del Castigo Póstumo no se entienden realmente bien.",
+  "DESC_TRACKING": "Duración del Rastreador y la Bomba Trampa",
+  "DESC_RECOVERY_TIME": "{{value}}s para llenar el tanque",
+  "DESC_MAIN_COST": "{{totalShots}} para vaciar el tanque ({{reduction}}% de reducción)",
+  "DESC_SUB_COST": "{{reduction}}% de reducción",
+  "DESC_SPECIAL_COST": "{{value}}p para el especial",
   "DESC_DISTANCE": "{{value}} Distance Units",
-  "DESC_QUICK_RESPAWN": "Respawn time when splatted without splatting others.",
+  "DESC_QUICK_RESPAWN": "Reaparición más rápida cuando te liquidan sin que tu hayas liquidado a alguien varias veces.",
   "UNIT_DISTANCE_UNITS_PER_FRAME": "Distance Units/frame",
 
   // The "unit" weapons fire in
   // Used in phrases like "200 shots to empty"
-  "SHOT_UNIT_SHOT": "shot",
-  "SHOT_UNIT_SHOT_PLURAL": "shots",
-  "SHOT_UNIT_FLICK": "flick",
-  "SHOT_UNIT_FLICK_PLURAL": "flicks",
-  "SHOT_UNIT_FLICK_HORIZ": "horizontal flick",
-  "SHOT_UNIT_FLICK_HORIZ_PLURAL": "horizontal flicks",
-  "SHOT_UNIT_FLICK_VERT": "vertical flick",
-  "SHOT_UNIT_FLICK_VERT_PLURAL": "vertical flicks",
-  "SHOT_UNIT_SLOSHER": "slosh",
-  "SHOT_UNIT_SLOSHER_PLURAL": "sloshes",
-  "SHOT_UNIT_CHARGER": "full charge",
-  "SHOT_UNIT_CHARGER_PLURAL": "full charges",
+  "SHOT_UNIT_SHOT": "Disparo",
+  "SHOT_UNIT_SHOT_PLURAL": "Disparos",
+  "SHOT_UNIT_FLICK": "Barrido",
+  "SHOT_UNIT_FLICK_PLURAL": "Barridos",
+  "SHOT_UNIT_FLICK_HORIZ": "¡Barrido horizontal",
+  "SHOT_UNIT_FLICK_HORIZ_PLURAL": "Barridos horizontales",
+  "SHOT_UNIT_FLICK_VERT": "Barrido vertical",
+  "SHOT_UNIT_FLICK_VERT_PLURAL": "Barridos verticales",
+  "SHOT_UNIT_SLOSHER": "Cubazo",
+  "SHOT_UNIT_SLOSHER_PLURAL": "Cubazos",
+  "SHOT_UNIT_CHARGER": "Disparo cargado",
+  "SHOT_UNIT_CHARGER_PLURAL": "Disparos cargados",
 
   // Future feature - ink breakdown
   //// Combined with SHOT_UNIT
@@ -170,10 +170,10 @@ var es_strings = {
 
   // This goes in the "about" popup.
   /// Leave the HTML tags alone.
-  "UI_ABOUT_CONTENT": `Built primarily with AngularJS 1.6.5 and Bootstrap 3.<br><br>
-  This tool was developed using info and assets datamined by <a href="https://twitter.com/LeanYoshi" target="_blank">Lean</a>, as well as information provided by several members of the Inkademy and Splatoon Developers servers on Discord.
-  <br><br>Thanks to everyone who gave me help and data!<br><br>
-  Many of the formulas used can be seen on <a href="https://splatoonwiki.org/wiki/User:Heddy/Charts" target="_blank">Heddy's charts</a> on Inkipedia.<br><br>
+  "UI_ABOUT_CONTENT": `Creado principalmente con AngularJS 1.6.5 y Bootstrap 3.<br><br>
+ Esta herramienta fue desarrollada utilizando información y recursos que puedes encontrar en <a href="https://twitter.com/LeanYoshi" target="_blank">Lean</a>, así como información proporcionada por varios miembros de  Inkademy y Splatoon Server Developers  en Discord.
+  <br><br>¡Gracias a todos los que me prestaron su ayuda y datos!<br><br>
+  Muchas de las fórmulas usadas puedes verlas en <a href="https://splatoonwiki.org/wiki/User:Heddy/Charts" target="_blank">Heddy's charts</a> on Inkipedia.<br><br>
   Any feature suggestions, bug reports, and feedback can be left as issues on the project's <a href="https://github.com/DeviPotato/splat2-calc" target="_blank">GitHub</a>. <br>
   You can also reach me on the project's <a href="https://twitter.com/loadout_ink" target="_blank">twitter</a>.`
 }
